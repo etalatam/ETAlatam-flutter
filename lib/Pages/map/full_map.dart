04791022d81648a412932547334dedf03c7d8487
Robert Bruno <robert.bruno@sigis.com.ve>
2024-10-14 08:19:11 -0400

    fix: update map view

--- lib/Pages/map/full_map.dart
+++ lib/Pages/map/full_map.dart
@@ -15,45 +15,35 @@ class FullMapState extends State<FullMap> {
   _onMapCreated(MapboxMap mapboxMap) {
     this.mapboxMap = mapboxMap;
 
-    this.mapboxMap?.logo
+    this
+        .mapboxMap
+        ?.logo
         .updateSettings(LogoSettings(marginRight: 3000, marginTop: 3000));
     this.mapboxMap?.attribution.updateSettings(AttributionSettings(
         marginRight: 3000, marginTop: 3000, clickable: false));
-        
-    this.mapboxMap?.location.updateSettings(
-        LocationComponentSettings(
+
+    this.mapboxMap?.location.updateSettings(LocationComponentSettings(
           enabled: true,
           pulsingEnabled: true,
           showAccuracyRing: true,
           puckBearingEnabled: true,
+        ));
 
-        )
-    );
-    
-    this.mapboxMap?.on UserLocationUpdated
+    // this.mapboxMap?.on UserLocationUpdated
 
     this.mapboxMap?.setCamera(CameraOptions(
-            padding: MbxEdgeInsets(top: 1, left: 2, bottom: 3, right: 4),
-            anchor: ScreenCoordinate(x: 1, y: 1),
-            zoom: 14));
+        padding: MbxEdgeInsets(top: 1, left: 2, bottom: 3, right: 4),
+        anchor: ScreenCoordinate(x: 1, y: 1),
+        zoom: 14));
     // MbxEdgeInsets(top: 1, left: 2, bottom: 3, right: 4),
     this.mapboxMap?.scaleBar.updateSettings(ScaleBarSettings(
-      enabled: true,
-      position: OrnamentPosition.BOTTOM_LEFT
-    ));
+        enabled: true, position: OrnamentPosition.BOTTOM_LEFT));
     this.mapboxMap?.compass.updateSettings(CompassSettings(
-        enabled: true,
-        position: OrnamentPosition.BOTTOM_RIGHT
-    ));
-
-
+        enabled: true, position: OrnamentPosition.BOTTOM_RIGHT));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: MapWidget(
-          onMapCreated: _onMapCreated
-    ));
+    return Scaffold(body: MapWidget(onMapCreated: _onMapCreated));
   }
 }
