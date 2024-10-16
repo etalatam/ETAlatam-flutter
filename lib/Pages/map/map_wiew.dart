043c74314622b7bba6a9b6de41fbece394879fa1
Robert Bruno <robert.bruno@sigis.com.ve>
2024-10-16 14:28:59 -0400

    fix: add tracking map

--- lib/Pages/map/map_wiew.dart
+++ lib/Pages/map/map_wiew.dart
@@ -14,10 +14,14 @@ class MapWiew extends StatefulWidget {
 class MapWiewState extends State<MapWiew> {
   MapboxMap? mapboxMap;
 
-  _onMapCreated(MapboxMap mapboxMap) {
-    this.mapboxMap = mapboxMap;
+  LocationService? locationService;
 
-    final locationService = Provider.of<LocationService>(context, listen: true);
+  bool _firstLocationUpdate = true;
+
+  _onMapCreated(MapboxMap mapboxMap) {
+    print('[MapView._onMapCreated]');
+
+    this.mapboxMap = mapboxMap;
 
     this
         .mapboxMap
@@ -35,15 +39,15 @@ class MapWiewState extends State<MapWiew> {
 
     // this.mapboxMap?.on UserLocationUpdated
 
-    this.mapboxMap?.setCamera(CameraOptions(
-        center: Point(
-            coordinates: Position(
-          locationService.locationData?['longitude'],
-          locationService.locationData?['latitude'],
-        )),
-        padding: MbxEdgeInsets(top: 1, left: 2, bottom: 3, right: 4),
-        anchor: ScreenCoordinate(x: 1, y: 1),
-        zoom: 5));
+    // this.mapboxMap?.setCamera(CameraOptions(
+    //     center: Point(
+    //         coordinates: Position(
+    //       locationService?.locationData?['longitude'],
+    //       locationService?.locationData?['latitude'],
+    //     )),
+    //     padding: MbxEdgeInsets(top: 1, left: 2, bottom: 3, right: 4),
+    //     anchor: ScreenCoordinate(x: 1, y: 1),
+    //     zoom: 5));
     // MbxEdgeInsets(top: 1, left: 2, bottom: 3, right: 4),
     this.mapboxMap?.scaleBar.updateSettings(ScaleBarSettings(
         enabled: true, position: OrnamentPosition.BOTTOM_LEFT));
@@ -53,6 +57,44 @@ class MapWiewState extends State<MapWiew> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: MapWidget(onMapCreated: _onMapCreated));
+    print('[MapView.build]');
+    return Scaffold(body:
+        Consumer<LocationService>(builder: (context, locationService, child) {
+      final locationData = locationService.locationData;
+      print('[MapView.Consumer.LocationService] $locationData');
+      if (locationData != null && mapboxMap != null) {
+        if (_firstLocationUpdate) {
+          mapboxMap?.setCamera(CameraOptions(
+            zoom: 14,
+            pitch: 60,
+            center: Point(
+                coordinates: Position(
+                    locationData?['longitude'], locationData?['latitude'])),
+          ));
+          _firstLocationUpdate = false;
+        } else {
+          mapboxMap?.setCamera(CameraOptions(
+            center: Point(
+                coordinates: Position(
+                    locationData?['longitude'], locationData?['latitude'])),
+          ));
+        }
+      }
+
+      return MapWidget(
+          styleUri: MapboxStyles.MAPBOX_STREETS,
+          onMapCreated: _onMapCreated,
+          cameraOptions: CameraOptions(
+              center: Point(
+                  coordinates: Position(-79.50451720694494, 9.055044879215595)),
+              zoom: 5.0));
+    }));
+  }
+
+  @override
+  void initState() {
+    print('[MapView.initState]');
+    super.initState();
+    locationService = Provider.of<LocationService>(context, listen: false);
   }
 }
