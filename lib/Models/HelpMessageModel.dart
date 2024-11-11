1881f4a60bc35e464d4903f9ce5c04a34c38090c
Robert Bruno <robert.bruno@sigis.com.ve>
2024-11-11 10:03:05 -0400

    fix: update CommentModel

--- lib/Models/HelpMessageModel.dart
+++ lib/Models/HelpMessageModel.dart
@@ -1,24 +1,17 @@
 import 'dart:core';
 
-
 class SupportHelpCategory {
   int? id;
   String? name;
 
-  SupportHelpCategory({
-    this.id,
-    this.name
-  });
+  SupportHelpCategory({this.id, this.name});
 
-  factory SupportHelpCategory.fromJson( json ) {
-    return SupportHelpCategory(
-      id: json['id'],
-      name: json['name']
-    );
+  factory SupportHelpCategory.fromJson(json) {
+    return SupportHelpCategory(id: json['id'], name: json['name']);
   }
 }
+
 class HelpMessageModel {
-  
   int? message_id;
   String? title;
   String? message;
@@ -42,36 +35,35 @@ class HelpMessageModel {
     this.date,
     this.comments,
   });
-  
-  factory HelpMessageModel.fromJson( json) {
 
+  factory HelpMessageModel.fromJson(json) {
     List<CommentModel>? comments = [];
-   
+
     try {
       Iterable commentsList = json['comments'];
-      comments = json['comments'] != null ?  List<CommentModel>.from(commentsList.map((model)=>  CommentModel.fromJson(model))) : [];      
+      comments = json['comments'] != null
+          ? List<CommentModel>.from(
+              commentsList.map((model) => CommentModel.fromJson(model)))
+          : [];
     } catch (e) {
       print(e.toString());
     }
 
     return HelpMessageModel(
-      message_id: json['id'] as int?,
-      // title: json['subject'] as String?,
-      title: json['category_name'] as String?,
-      message: json['content'] as String?,
-      status: "status", //json['status'] as String?,
-      priority: "${json['priority_id']}" as String?,
-      user_id: json['id_driver'] as int,
-      short_date: json['ts'] as String?,
-      date: json['ts'] as String?,
-      comments: comments
-    );
+        message_id: json['id'] as int?,
+        // title: json['subject'] as String?,
+        title: json['category_name'] as String?,
+        message: json['content'] as String?,
+        status: "status", //json['status'] as String?,
+        priority: "${json['priority_id']}" as String?,
+        user_id: json['id_driver'] as int,
+        short_date: json['ts'] as String?,
+        date: json['ts'] as String?,
+        comments: comments);
   }
-
 }
 
 class CommentModel {
-  
   int? id;
   String? comment;
   String? message_id;
@@ -89,25 +81,29 @@ class CommentModel {
     this.date,
     this.user,
   });
-  
-  factory CommentModel.fromJson( json) {
 
-    CommentUserModel user = json['user'] != null ? CommentUserModel.fromJson(json['user']) : CommentUserModel();
+  factory CommentModel.fromJson(json) {
+    CommentUserModel? user;
+    try {
+      user = json['user'] != null
+          ? CommentUserModel.fromJson(json['user'])
+          : CommentUserModel();
+    } catch (e) {
+      print(e.toString());
+    }
 
     return CommentModel(
       id: json['id'] as int?,
-      comment: json['comment'] as String?,
-      user_id: json['user_id'] as int,
-      short_date: json['short_date'] as String?,
-      date: json['date'] as String?,
+      comment: json['txt'] as String?,
+      user_id: json['id_usu'] as int,
+      short_date: json['ts'] as String?,
+      date: json['ts'] as String?,
       user: user,
     );
   }
-
 }
 
 class CommentUserModel {
-  
   String? name;
   String? email;
   String? photo;
@@ -117,15 +113,12 @@ class CommentUserModel {
     this.email,
     this.photo,
   });
-  
-  factory CommentUserModel.fromJson( json) {
+
+  factory CommentUserModel.fromJson(json) {
     return CommentUserModel(
       name: json['first_name'] as String?,
       email: json['email'] as String?,
       photo: '/uploads/images/60x60.png',
     );
   }
-
 }
-
-
