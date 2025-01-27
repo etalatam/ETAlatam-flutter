ac089d33230eabcec3c27c5b66b768894dc212fc
Robert Bruno <robert.alexander.bruno@gmail.com>
2025-01-26 22:49:06 -0400

    fix: update trips order

--- lib/API/client.dart
+++ lib/API/client.dart
@@ -422,7 +422,7 @@ class HttpService {
   Future<List<TripModel>> getGuardianTrips(String active) async {
     const endpoint = "/rpc/guardian_trips";
     http.Response res =
-        await getQuery("$endpoint?select=*&limit=10&running=eq.$active");
+        await getQuery("$endpoint?select=*&limit=10&running=eq.$active&order=id_trip.desc");
 
     print("[$endpoint] res.statusCode: ${res.statusCode}");
     print("[$endpoint] res.body: ${res.body}");
