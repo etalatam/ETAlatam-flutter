51d3f67fe938ebc33810470c4e37b29c554cc7a6
Robert Bruno <robert.alexander.bruno@gmail.com>
2025-02-17 22:02:45 -0400

    fix: update

--- lib/shared/location/location_service.dart
+++ lib/shared/location/location_service.dart
@@ -118,7 +118,7 @@ class LocationService extends ChangeNotifier {
         autoStop: false,
         androidSettings:  AndroidSettings(
             accuracy: LocationAccuracy.NAVIGATION,
-            interval: relationNameLocal == 'wta.drivers' ? 5 : 60,
+            interval: relationNameLocal.contains('eta.drivers') ? 5 : 60,
             distanceFilter: 0,
             client: LocationClient.google,
             androidNotificationSettings: AndroidNotificationSettings(
